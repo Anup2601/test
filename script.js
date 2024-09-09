8ccbe03d52a459faf243759b181f8b315a60ed8d
CypherNinja1010 <priyesh.kumarjii@gmail.com>
2024-09-09 23:13:38 +0530

    Files setting changed

--- script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('feedbackForm').addEventListener('submit', function(e) {
-    e.preventDefault();  
-    document.getElementById('thankYouMessage').style.display = 'block';  
-    document.getElementById('email').value = '';  
-    document.getElementById('feedbackText').value = '';  
-  });
-  
